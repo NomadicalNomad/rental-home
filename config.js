c278c4eae919e0fac17cbe4c34135df246844e3d
NomadicalNomad <44391695+NomadicalNomad@users.noreply.github.com>
2026-09-19 10:30:24 -0700

    chore: add Pages config.js with Supabase anon key

---
+++ config.js
@@ -0,0 +1,4 @@
+window.RENTAL_HOME_CONFIG = {
+    supabaseUrl: 'https://khcwyxjhuayktrddodzg.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoY3d5eGpodWF5a3RyZGRvZHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4MzIzMjksImV4cCI6MjEwNTQwODMyOX0.KaZg12T0MWDB16Iy9i25vKhQ84XEA8qJfoGhRjsbmNA'
+};
